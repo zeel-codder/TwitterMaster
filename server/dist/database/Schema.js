6785ade3b4ea6268cbc3f0564ba8ed64ae5366e8
zeel-codder <zeelprajapati321@gmail.com>
2021-10-12 19:32:44 +0530

    add the video and file uploader

---
+++ server/dist/database/Schema.js
@@ -0,0 +1,39 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GroupModel = exports.TweetModel = exports.UserModel = void 0;
+var mongoose_1 = __importDefault(require("mongoose"));
+var Schema = mongoose_1.default.Schema, model = mongoose_1.default.model;
+var UserSchema = new Schema({
+    name: String,
+    email: String,
+    image: String,
+    password: String,
+    followers: [{ type: String }],
+    follow: [{ type: String }]
+}, { versionKey: false });
+var TweetSchema = new Schema({
+    title: String,
+    description: String,
+    like: [{ type: String }],
+    retweet: Number,
+    explore: [{ type: String }],
+    image: String,
+    video: String,
+    Creator_ID: String,
+    Creator_Name: String
+}, { versionKey: false });
+var GroupSchema = new Schema({
+    title: String,
+    description: String,
+    admin: [{ type: String }],
+    users: [{ type: String }],
+}, { versionKey: false });
+var UserModel = model('User', UserSchema);
+exports.UserModel = UserModel;
+var TweetModel = model('Tweet', TweetSchema);
+exports.TweetModel = TweetModel;
+var GroupModel = model('Group', GroupSchema);
+exports.GroupModel = GroupModel;
---
+++ server/dist/routes/tweet.js
@@ -0,0 +1,41 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var CRUD_1 = require("../controllers/tweet/CRUD");
+var Auth_1 = require("../middlewares/Auth");
+var router = express_1.default.Router();
+// middleware that is specific to this router
+// router.use(function timeLog (req, res, next) {
+//   console.log('Time: ', Date.now())
+//   next()
+// })
+// console.log('tweet')
+var multer_1 = __importDefault(require("multer"));
+var storage = multer_1.default.diskStorage({
+    destination: function (req, file, cb) {
+        console.log(file);
+        cb(null, "./" + process.env.upload + "/files");
+        // cb(null, path.join('../files'))
+    },
+    filename: function (req, file, cb) {
+        var uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, new Date().toISOString() + file.originalname);
+        // cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+});
+var upload = multer_1.default({ storage: storage });
+router.get('/', function (req, res) {
+    CRUD_1.GetTweets(req, res);
+});
+router.post('/create', Auth_1.Auth, upload.single('media'), CRUD_1.AddTweet);
+router.delete('/delete', function (req, res) {
+    CRUD_1.DeleteTweet(req, res);
+});
+router.put('/update', Auth_1.Auth, CRUD_1.UpdateTweet);
+router.get('/:_id', function (req, res) {
+    CRUD_1.GetTweet(req, res);
+});
+exports.default = router;
